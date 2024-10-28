306ef5cb0c7f05d65747a486f5bdcd6e1016c518
indigoxela <user@olga.indigofloat.at>
2024-10-28 10:44:29 +0100

    Issue #12: PHP compatibility of mandrill/mandrill

--- libraries/mandrill/src/Mandrill/Internal.php
+++ libraries/mandrill/src/Mandrill/Internal.php
@@ -1,6 +1,9 @@
 <?php
 
 class Mandrill_Internal {
+
+    public $master;
+
     public function __construct(Mandrill $master) {
         $this->master = $master;
     }
--- libraries/mandrill/src/Mandrill/Webhooks.php
+++ libraries/mandrill/src/Mandrill/Webhooks.php
@@ -1,6 +1,9 @@
 <?php
 
 class Mandrill_Webhooks {
+
+    public $master;
+
     public function __construct(Mandrill $master) {
         $this->master = $master;
     }
